7f82098ddd956ad350913c95282e24d4ac99627b
YukiNekoNeko <t236058@st.yamagata-u.ac.jp>
2024-11-08 19:00:56 +0900

    Add files via upload

---
+++ kadai5.c
@@ -0,0 +1,11 @@
+#include<studio.h>
+#include<float.h>
+#include<limits.h>
+int main(void)
+{
+printf("char型の最大値:%d\n",UCHAR_MAX);
+printf("char型の最大値:%d\n",CHAR_MAX);
+printf("char型の最大値:%d\n",CHAR_MIN);
+
+return 0;
+}
---
+++ 無題3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int nyan;
+	char large;
+	char small;
+
+	printf("小文字を入力");
+	scanf("%c",&small);
+	nyan =  'a'+ 'A';
+	large = -small + nyan;
+	
+	printf("大文字は%c 小文字は%c\n", large, small);
+	nyan = 1
+	printf("大文字は%c 小文字は%c\n","ねこは%d", large, small,nyan);
+	
+	return 0;
+}
